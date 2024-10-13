1eb16abb78a937d0cbe3126d338db94e968a84eb
Pasha <pavik.varganov@gmail.com>
2024-10-13 11:04:42 +0300

    add list serializers in 'station' app

--- station/serializers.py
+++ station/serializers.py
@@ -30,6 +30,15 @@ class RouteSerializer(serializers.ModelSerializer):
         fields = ("id", "source", "destination", "distance")
 
 
+class RouteListSerializer(RouteSerializer):
+    source_station = serializers.CharField(source="source.name", read_only=True)
+    destination_station = serializers.CharField(source="destination.name", read_only=True)
+
+    class Meta:
+        model = Route
+        fields = ("id", "source_station", "destination_station", "distance")
+
+
 class TrainTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrainType
@@ -42,10 +51,32 @@ class TrainSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "cargo_num", "places_in_cargo", "train_type")
 
 
+class TrainListSerializer(TrainSerializer):
+    train_type = serializers.CharField(source="train_type.name", read_only=True)
+
+
 class JourneySerializer(serializers.ModelSerializer):
     class Meta:
         model = Journey
-        fields = ("id", "route", "train", "departure_time", "arrival_time")
+        fields = ("id", "route", "train", "departure_time", "arrival_time", "crew")
+
+
+class JourneyListSerializer(JourneySerializer):
+    train_name = serializers.CharField(source="train.name", read_only=True)
+    train_capacity = serializers.IntegerField(
+        source="train.capacity", read_only=True
+    )
+    crew = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="full_name"
+    )
+    route_path = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Journey
+        fields = ("id", "route_path", "train_name", "train_capacity", "departure_time", "arrival_time", "crew")
+
+    def get_route_path(self, obj):
+        return f"{obj.route.source.name} - {obj.route.destination.name}"
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -54,8 +85,17 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ("id", "created_at", "user")
 
 
+class OrderListSerializer(OrderSerializer):
+    user = serializers.CharField(source="user.username", read_only=True)
+
+
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = ("id", "cargo", "seat", "journey", "order")
+
+
+class TicketListSerializer(TicketSerializer):
+    journey = serializers.CharField(source="journey.route", read_only=True)
+    order = OrderListSerializer(read_only=True)
 
--- station/views.py
+++ station/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.db.models import F
 from rest_framework import viewsets
 
 from station.models import (
@@ -20,6 +20,7 @@ from station.serializers import (
     JourneySerializer,
     OrderSerializer,
     TicketSerializer,
+    RouteListSerializer, TrainListSerializer, JourneyListSerializer, OrderListSerializer, TicketListSerializer,
 )
 
 
@@ -34,8 +35,13 @@ class StationViewSet(viewsets.ModelViewSet):
 
 
 class RouteViewSet(viewsets.ModelViewSet):
-    queryset = Route.objects.all()
-    serializer_class = RouteSerializer
+    queryset = Route.objects.select_related("source", "destination")
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+
+        return RouteSerializer
 
 
 class TrainTypeViewSet(viewsets.ModelViewSet):
@@ -44,20 +50,41 @@ class TrainTypeViewSet(viewsets.ModelViewSet):
 
 
 class TrainViewSet(viewsets.ModelViewSet):
-    queryset = Train.objects.all()
-    serializer_class = TrainSerializer
+    queryset = Train.objects.select_related("train_type")
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TrainListSerializer
+
+        return TrainSerializer
 
 class JourneyViewSet(viewsets.ModelViewSet):
-    queryset = Journey.objects.all()
-    serializer_class = JourneySerializer
+    queryset = (
+        Journey.objects
+        .select_related("route", "train")
+        .prefetch_related("crew")
+    )   # 27 -> 23 -> 20
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return JourneyListSerializer
+
+        return JourneySerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+    queryset = Order.objects.select_related("user")
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+
+        return OrderSerializer
 
 class TicketViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
+    queryset = Ticket.objects.select_related("journey__route", "order__user")
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TicketListSerializer
+
+        return TicketSerializer
