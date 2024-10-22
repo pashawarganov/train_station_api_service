56b717ae12223fcc4dc6e7dc1a943f97c759c968
Pasha <pavik.varganov@gmail.com>
2024-10-22 16:44:05 +0300

    install flake8-quotes and fix relation problems

--- train_station_api_service/urls.py
+++ train_station_api_service/urls.py
@@ -23,7 +23,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/station/", include("station.urls", namespace="station")),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
